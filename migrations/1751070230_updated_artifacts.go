ef360ae14ea667b3c7528b4a784a7ec4871b3707
defektive <defektive@users.noreply.github.com>
2025-06-27 18:24:00 -0600

    make it required

---
+++ migrations/1751070230_updated_artifacts.go
@@ -0,0 +1,60 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1303748624")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(3, []byte(`{
+			"hidden": false,
+			"id": "file698238600",
+			"maxSelect": 99,
+			"maxSize": 0,
+			"mimeTypes": [],
+			"name": "artifacts",
+			"presentable": true,
+			"protected": true,
+			"required": true,
+			"system": false,
+			"thumbs": [],
+			"type": "file"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1303748624")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(3, []byte(`{
+			"hidden": false,
+			"id": "file698238600",
+			"maxSelect": 99,
+			"maxSize": 0,
+			"mimeTypes": [],
+			"name": "artifacts",
+			"presentable": true,
+			"protected": true,
+			"required": false,
+			"system": false,
+			"thumbs": [],
+			"type": "file"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
